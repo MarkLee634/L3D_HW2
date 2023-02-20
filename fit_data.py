f6ffd3effa51caa3e1a9d78a48af507f98583f09
Mark Lee <lmy0510@gmail.com>
2023-02-20 14:24:17 -0500

    debugging needed for Q1. pred slow, and not showing

--- fit_data.py
+++ fit_data.py
@@ -10,11 +10,75 @@ from pytorch3d.structures import Meshes
 import dataset_location
 import torch
 
+import sys
+import matplotlib.pyplot as plt
+import mcubes
+import numpy as np
+import pytorch3d
+import imageio
+from mpl_toolkits.axes_grid1 import ImageGrid
+
+import utils_vox
+
+def render_mesh_from_voxels(voxels, device, gif_save, output_path='/home/mark/course/16825L43D/L3D_HW2/images/X.gif'):
+    """
+    Renders a sphere using parametric sampling. Samples num_samples ** 2 points.
+    """
+    image_size=256
+    voxel_size = 32
+
+    if device is None:
+        device = utils_vox.get_device()
+
+    min_value = -1.1
+    max_value = 1.1
+
+    vertices, faces = mcubes.marching_cubes(mcubes.smooth(voxels), isovalue=0)
+    vertices = torch.tensor(vertices).float()
+    faces = torch.tensor(faces.astype(int))
+    # Vertex coordinates are indexed by array position, so we need to
+    # renormalize the coordinate system.
+    vertices = (vertices / voxel_size) * (max_value - min_value) + min_value
+    textures = (vertices - vertices.min()) / (vertices.max() - vertices.min())
+    textures = pytorch3d.renderer.TexturesVertex(vertices.unsqueeze(0))
+
+    mesh = pytorch3d.structures.Meshes([vertices], [faces], textures=textures).to(
+        device
+    )
+
+    NUM_VIEWS = 36
+    # transform the camera around the obj 360 degrees
+    azim = torch.linspace(-180, 180, NUM_VIEWS)
+
+    lights = pytorch3d.renderer.PointLights(location=[[0, 0.5, -4.0]], device=device)
+    renderer = utils_vox.get_mesh_renderer(image_size=image_size, device=device)
+    rend_list = []
 
 
+    R, T = pytorch3d.renderer.look_at_view_transform(dist=10, elev=0, azim=180)
+    cameras = pytorch3d.renderer.FoVPerspectiveCameras(R=R, T=T, device=device)
+    rend = renderer(mesh, cameras=cameras, lights=lights)
+    
+
+    if gif_save == True:
+        for i in range (NUM_VIEWS):
+            # Prepare the camera:
+            # specify elevation and azimuth angles for each viewpoint as tensors. 
+            R, T = pytorch3d.renderer.look_at_view_transform(dist=5.0, elev=30, azim=azim[i])
+            cameras = pytorch3d.renderer.FoVPerspectiveCameras(device=device, R=R, T=T)
+            rend = renderer(mesh, cameras=cameras, lights=lights)
+            rend = rend.cpu().numpy()[:, ..., :3]
+            rend = rend[0]
 
 
+            rend_list.append(rend)
+        
 
+        imageio.mimsave(output_path, rend_list, fps=15)
+
+    return rend[0, ..., :3].detach().cpu().numpy().clip(0, 1)
+
+    
 def get_args_parser():
     parser = argparse.ArgumentParser('Model Fit', add_help=False)
     parser.add_argument('--lr', default=4e-4, type=float)
@@ -26,6 +90,20 @@ def get_args_parser():
     parser.add_argument('--w_smooth', default=0.1, type=float)
     parser.add_argument('--device', default='cuda', type=str) 
     return parser
+
+def grid_plot_pred_label(init_pred, optimized_pred, label):
+
+    fig = plt.figure(figsize=(4., 4.))
+    grid = ImageGrid(fig, 111,  # similar to subplot(111)
+                    nrows_ncols=(2, 2),  # creates 2x2 grid of axes
+                    axes_pad=0.1,  # pad between axes in inch.
+                    )
+
+    for ax, im in zip(grid, [init_pred, label, optimized_pred , label]):
+        # Iterating over the grid returns the Axes.
+        ax.imshow(im)
+
+    plt.show()
 
 def fit_mesh(mesh_src, mesh_tgt, args):
     start_iter = 0
@@ -90,10 +168,15 @@ def fit_voxel(voxels_src, voxels_tgt, args):
     start_iter = 0
     start_time = time.time()    
     optimizer = torch.optim.Adam([voxels_src], lr = args.lr)
+
+
     for step in range(start_iter, args.max_iter):
         iter_start_time = time.time()
 
+        # print(f"------------ voxels_src.shape {voxels_src.shape}, {voxels_tgt.shape} ------------ ")
         loss = losses.voxel_loss(voxels_src,voxels_tgt)
+        # print(f"------------ loss {loss} ------------ ")
+        
 
         optimizer.zero_grad()
         loss.backward()
@@ -101,12 +184,30 @@ def fit_voxel(voxels_src, voxels_tgt, args):
 
         total_time = time.time() - start_time
         iter_time = time.time() - iter_start_time
-
         loss_vis = loss.cpu().item()
 
+        #save the first iteration
+        if step == 0:
+            # render of the unoptimized src
+            rendered_img_init = render_mesh_from_voxels(voxels_src[0].detach().cpu().numpy(), args.device, gif_save = False)
+            # render of the target
+            rendered_img_target = render_mesh_from_voxels(voxels_tgt[0].detach().cpu().numpy(), args.device, gif_save = False)
+
+
         print("[%4d/%4d]; ttime: %.0f (%.2f); loss: %.3f" % (step, args.max_iter, total_time,  iter_time, loss_vis))
-    
+
     print('Done!')
+
+    # render optimized src
+    output_path = '/home/mark/course/16825L43D/L3D_HW2/images/voxel_target.gif'
+    rendered_img_opt = render_mesh_from_voxels(voxels_src[0].detach().cpu().numpy(), args.device, gif_save = False, output_path = output_path)
+
+    # plot all 4 images 
+    grid_plot_pred_label(rendered_img_init, rendered_img_opt, rendered_img_target)
+
+
+
+    sys.exit()
 
 
 def train_model(args):
--- losses.py
+++ losses.py
@@ -1,12 +1,37 @@
 import torch
+import numpy as np
 
 # define losses
 def voxel_loss(voxel_src,voxel_tgt):
 	# voxel_src: b x h x w x d
 	# voxel_tgt: b x h x w x d
-	# loss = 
-	# implement some loss for binary voxel grids
-	return loss
+
+	#get 1 batch
+	voxel_src = voxel_src[0]
+	voxel_tgt = voxel_tgt[0]
+
+	#flatten Width x Height x Depth into 1D array
+	voxel_src_flat = voxel_src.flatten()
+	voxel_tgt_flat = voxel_tgt.flatten()
+
+	#define loss function
+	loss = torch.nn.BCELoss()
+
+	#average BCE loss over all voxels
+	number_voxels = voxel_src_flat.shape[0]
+	loss_voxel = 0
+
+
+	for i in range(number_voxels):
+
+		#clamp values between 0 and 1
+
+		loss_voxel += loss(torch.clamp(voxel_src_flat[i],min=0.,max=1.),torch.clamp(voxel_tgt_flat[i],min=0.,max=1.))
+		
+
+	loss_voxel = loss_voxel/number_voxels
+
+	return loss_voxel
 
 def chamfer_loss(point_cloud_src,point_cloud_tgt):
 	# point_cloud_src, point_cloud_src: b x n_points x 3  
